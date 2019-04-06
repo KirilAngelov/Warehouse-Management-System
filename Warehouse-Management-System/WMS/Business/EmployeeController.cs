53e6859c492e09175981097d688a345a91ab12a4
Momchil <momchilgenov2001@gmail.com>
2019-04-06 18:07:25 +0300

    did unit tests and finishing up project

--- Warehouse-Management-System/WMS/Business/EmployeeController.cs
+++ Warehouse-Management-System/WMS/Business/EmployeeController.cs
@@ -101,6 +101,7 @@ namespace WMS.Business
             }
         }
 
+
       
 
     }
---
+++ Warehouse-Management-System/WMS/TestClass.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using WMS.Business;
+using WMS.Data;
+using WMS.Data.Models;
+namespace WMS
+{
+
+    [TestFixture]
+    public class TestClass
+    {
+
+        [TestCase]
+        public void GetItem(int id)
+        {
+            Item item = new Item("Gold Ingot", "03052018", 15000, null, 310, 5);
+            EmployeeController employee = new EmployeeController();
+            Assert.AreEqual(employee.GetItem(24), item);
+        }
+        [TestCase]
+        public void GetItemID(string itemname)
+        {
+            Item item = new Item("Gold Ingot", "03052018", 15000, null, 310, 5);
+            EmployeeController employee = new EmployeeController();
+            Assert.AreEqual(employee.GetItemId(itemname), item.Name_Of_Item);
+        }
+
+        [TestCase]
+        public void GetEmployeeID(string firstName, string lastName)
+        {
+            Employee employee2 = new Employee("John", "Carpenter", 25, 5, 2000, "bearer");
+            EmployeeController employee = new EmployeeController();
+            Assert.AreEqual(employee.GetEmployeeId("John", "Carpenter"), 3);
+        }
+        [TestCase]
+        public void GetEmployee(int id)
+        {
+            Employee employee2 = new Employee("John", "Carpenter", 25, 5, 2000, "bearer");
+            EmployeeController employee = new EmployeeController();
+            Assert.AreEqual(employee.GetEmployee(id), employee2);
+        }
+
+        [TestCase]
+        public void GetFirstName(int id)
+        {
+            WorkerController worker = new WorkerController();
+            Assert.AreEqual(worker.GetFirstName(3), "John");
+        }
+        [TestCase]
+        public void GetLastName(int id)
+        {
+            WorkerController worker = new WorkerController();
+            Assert.AreEqual(worker.GetLastName(3), "Carpenter");
+        }
+        [TestCase]
+        public void GetAge(int id)
+        {
+            WorkerController worker = new WorkerController();
+            Assert.AreEqual(worker.GetAge(3), 23);
+        }
+        [TestCase]
+        public void GetPosition(int id)
+        {
+            WorkerController worker = new WorkerController();
+            Assert.AreEqual(worker.Position(id), "bearer");
+        }
+        [TestCase]
+        public void GetYearsOfService(int id)
+        {
+            WorkerController worker = new WorkerController();
+            Assert.AreEqual(worker.YearsOfService(id), 5);
+        }
+        [TestCase]
+        public void Getsalary(int id)
+        {
+            WorkerController worker = new WorkerController();
+            Assert.AreEqual(worker.Salary(id), 2000);
+        }
+        
+    }
+}
